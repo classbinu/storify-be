f09133de5fc0bb0b4b043bfb397ebc9ce0d395b3
JinJung0101 <qwe5344641@gmail.com>
2024-01-31 14:35:18 +0900

    친구신청 수정

--- src/noti/schema/noti.schema.ts
+++ src/noti/schema/noti.schema.ts
@@ -1,1 +1,30 @@
-export class Noti {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type NotiDocument = Noti & Document;
+
+@Schema()
+export class Noti {
+  @Prop({ required: true })
+  senderId: string;
+
+  @Prop({ required: true })
+  receiverId: string;
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({ required: true })
+  type: string;
+
+  @Prop({ required: true })
+  service: string; // 서비스 유형 필드 추가
+
+  @Prop({ default: 'unread' })
+  status: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const NotiSchema = SchemaFactory.createForClass(Noti);
