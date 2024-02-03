16f80734ff52e7518481f294d0151750a753ba32
JinJung0101 <qwe5344641@gmail.com>
2024-02-03 22:20:09 +0900

    노출된 토큰 삭제

--- test/socket-test.ts
+++ test/socket-test.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:3000/ws-noti/', {
   extraHeaders: {
-    authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWJlMjFiYTFlMzhjNTNhYjdjYjZmYmYiLCJpYXQiOjE3MDY5NjI3MDksImV4cCI6MTcwNzA0OTEwOX0.x8ALiCSllgxuyUFkdvu3onjDi8OpHB_2eUd8DO3MM70`,
+    authorization: `Bearer `,
   },
 });
 
--- test/socket-test2.ts
+++ test/socket-test2.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:3000/ws-noti/', {
   extraHeaders: {
-    authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWJlMjFjMTFlMzhjNTNhYjdjYjZmYzYiLCJpYXQiOjE3MDY5NjI3MjgsImV4cCI6MTcwNzA0OTEyOH0.kZtlNh5YVFC3wXi7e_4DHOzIPqdUwPQmxOamg-ZD5cw`,
+    authorization: `Bearer `,
   },
 });
 
