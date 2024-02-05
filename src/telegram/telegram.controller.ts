c7b3fb7727c1da491ce1d78789bf456901d8f53c
Classbinu <classbinu@gmail.com>
2024-02-05 19:53:47 +0900

    텔레그램 api 추가

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
+import { ApiTags } from '@nestjs/swagger';
+import { SendTelegramDto } from './dto/send-telegram.dto';
+import { TelegramService } from './telegram.service';
+
+@ApiTags('Telegram')
 @Controller('telegram')
-export class TelegramController {}
+export class TelegramController {
+  constructor(private readonly telegramService: TelegramService) {}
+
+  @Post()
+  async sendMessage(@Body() sendTelegramDto: SendTelegramDto): Promise<void> {
+    return this.telegramService.sendMessage(sendTelegramDto);
+  }
+}
