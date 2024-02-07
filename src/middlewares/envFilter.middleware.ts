646e2e697b638bed5672476456e031aa19f0b295
JinJung0101 <qwe5344641@gmail.com>
2024-02-07 20:54:07 +0900

    middleware 추가 - notFound.middleware추가, 잘못된 요청은 Not Found 처리 - helmet 모듈 추가하여 간단한 보안 강화

--- src/middlewares/envFilter.middleware.ts
+++ src/middlewares/envFilter.middleware.ts
@@ -4,7 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class EnvFilterMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (req.method === 'GET' && req.path.includes('.env')) {
+    if (
+      req.method === 'GET' &&
+      (req.path.includes('.env') || req.path.includes('admin'))
+    ) {
       res.status(403).send('Forbidden');
     } else {
       next();
