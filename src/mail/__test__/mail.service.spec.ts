bc6952f97748f960e908b2d2a285376ccab9268e
Classbinu <classbinu@gmail.com>
2024-02-08 15:52:05 +0900

    Mail Contoroller 테스트 케이스 작성

--- src/mail/__test__/mail.service.spec.ts
+++ src/mail/__test__/mail.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MailService } from './mail.service';
+
+import { MailService } from '../mail.service';
 
 describe('MailService', () => {
   let service: MailService;
--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,7 +1,7 @@
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { AccessTokenGuard } from 'src/auth/guards/accessToken.guard';
+import { AccessTokenGuard } from '../../src/auth/guards/accessToken.guard';
 import { FeedbackMailDto } from './dto/feedback-mail.dto';
 
 @ApiTags('mail')
@@ -14,6 +14,11 @@ export class MailController {
   @Post('feedback')
   async sendFeedbackMeil(@Req() req, @Body() feedbackMailDto: FeedbackMailDto) {
     const userId = req.user['sub'];
+
+    if (!feedbackMailDto.feedback) {
+      throw new Error('Feedback is required');
+    }
+
     return this.mailService.sendFeedbackMail(feedbackMailDto, userId);
   }
 }
