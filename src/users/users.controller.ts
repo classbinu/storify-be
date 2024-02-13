9aa81261a953397f4e3ea6f12af078d4e525ea96
JinJung0101 <qwe5344641@gmail.com>
2024-02-13 15:46:31 +0900

    카카오톡 로그인 수정 - 로그인 성공 시 홈페이지로 redirect 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -65,7 +65,7 @@ export class UsersController {
   })
   @UseInterceptors(
     FileInterceptor('avatar', {
-      limits: { fileSize: 2000000 }, // 2MB
+      limits: { fileSize: 4000000 }, // 4MB
       fileFilter: (req, file, callback) => {
         if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
           return callback(new Error('Only image files are allowed!'), false);
