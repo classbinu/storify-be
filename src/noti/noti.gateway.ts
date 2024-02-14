65c89b356b3b7ad655a856f23deeb4b75bf30d91
Classbinu <classbinu@gmail.com>
2024-02-14 22:31:30 +0900

    서버 배포

--- src/noti/noti.gateway.ts
+++ src/noti/noti.gateway.ts
@@ -18,6 +18,7 @@ import { NotiService } from './noti.service';
     allowedHeaders: ['authorization', 'Authorization'],
     credentials: true,
   },
+  namespace: 'ws-noti',
 })
 export class NotiGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
@@ -46,16 +47,36 @@ export class NotiGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
   }
 
+  // async handleConnection(client: Socket) {
+  //   const userId: string = await this.verifyToken(client);
+  //   const existingUser = this.users.get(userId);
+  //   if (existingUser) {
+  //     client.emit('message', 'You are already connected.');
+  //   } else {
+  //     this.users.set(userId, client.id);
+  //     this.users.set(client.id, userId);
+  //     client.emit('message', 'You have successfully connected.');
+  //   }
+  // }
+
   async handleConnection(client: Socket) {
-    const userId: string = await this.verifyToken(client);
-    const existingUser = this.users.get(userId);
-    if (existingUser) {
-      client.emit('message', 'You are already connected.');
-    } else {
-      this.users.set(userId, client.id);
-      this.users.set(client.id, userId);
-      client.emit('message', 'You have successfully connected.');
-    }
+    client.on('auth', async (token) => {
+      try {
+        const { sub } = await this.jwtService.decode(token);
+        const existingUser = this.users.get(sub);
+
+        if (existingUser) {
+          client.emit('message', 'You are already connected.');
+        } else {
+          this.users.set(sub, client.id);
+          this.users.set(client.id, sub);
+          client.emit('message', 'You have successfully connected.');
+        }
+      } catch (error) {
+        client.emit('error', 'Invalid token. Connection refused.');
+        client.disconnect();
+      }
+    });
   }
 
   async handleDisconnect(client: Socket) {
