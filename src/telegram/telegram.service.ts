bc5f36a291e97df8c8132c92045fa9ba16fa2336
Classbinu <classbinu@gmail.com>
2024-02-17 21:51:51 +0900

    텔레그램 서비스 레이어 테스트 코드 작성

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,18 +1,26 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 
 import { ConfigService } from '@nestjs/config';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { SendTelegramDto } from './dto/send-telegram.dto';
 
 @Injectable()
 export class TelegramService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject('TELEGRAM_BOT') private readonly telegramBot: TelegramBot,
+  ) {}
 
   async sendMessage(sendTelegramDto: SendTelegramDto): Promise<any> {
-    const token = this.configService.get<string>('TELEGRAM_BOT_TOKEN');
-    // const chatId = this.configService.get<string>('TELEGRAM_CHAT_ID');
     const channelId = this.configService.get<string>('TELEGRAM_CHANNEL_ID');
-    const bot = new TelegramBot(token);
-    return await bot.sendMessage(channelId, sendTelegramDto.message);
+
+    if (!sendTelegramDto.message) {
+      throw new Error('메시지를 입력해주세요.');
+    }
+
+    return await this.telegramBot.sendMessage(
+      channelId,
+      sendTelegramDto.message,
+    );
   }
 }
